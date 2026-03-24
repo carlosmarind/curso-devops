28c44b24c90a57c73ede703bce8d68c44eee9a29
Carlos Marin Diaz <carlosmarind@gmail.com>
2026-03-23 21:04:37 -0300

    muestra de conflicto

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
     return 'Hello World!!!!!';
-=======
-    return 'Hola Mundo!';
->>>>>>> feature-hi
   }
 
   getHi(): string {
