6731a82b799b14dd0ae72089f3de47360e6af22a
Carlos Marin Diaz <carlosmarind@gmail.com>
2026-03-25 20:02:18 -0300

    cambio en los mensajes de backend

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola Mundo!';
+    return 'Hola Mundo!!!!';
   }
 
   getHi(): string {
-    return 'Hi there!';
+    return 'Hi there!!!';
   }
 }
