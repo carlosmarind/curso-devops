ac5d89a93ca5383f1d7dc7e96601bc89bf10c576
Carlos Marin Diaz <carlosmarind@gmail.com>
2026-04-06 19:27:57 -0400

    cambio de prueba para ejecucion de pipeline

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Estamos en el ambiente ${process.env.NODE_ENV}`);
+  console.log(`Estamos en el ambiente correspondiente a ${process.env.NODE_ENV}`);
 }
 void bootstrap();
